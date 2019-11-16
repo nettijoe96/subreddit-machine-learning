4e7842e962ea203a82c3ad3594039fa0f18d97c3
Eric Tiano <etiano417@gmail.com>
2019-11-16 11:23:25 -0500

    Pickle the comment list

--- prawTest.py
+++ prawTest.py
@@ -1,6 +1,11 @@
 import time
 import praw
 import config
+import pickle
+
+DATA_FILE_NAME = "reddit_comments_list.pickle"
+#write binary mode
+WRITE_MODE = "wb"
 
 reddit = praw.Reddit(client_id=config.client_id,
                      client_secret=config.client_secret, password=config.password,
@@ -12,10 +17,20 @@ subIds = []
 for s in hot_python:
     subIds += [s]
 
+full_comment_list = list()
+
 for subId in subIds:
     submission = reddit.submission(id=subId)
     submission.comments.replace_more(limit=None)
-    for c in submission.comments.list():
+    comment_list = submission.comments.list()
+    for c in comment_list:
         print(c.body)
+    
+    full_comment_list = full_comment_list + comment_list
+    
+#create/identify the file to write reddit data to
+reddit_data_file = open(DATA_FILE_NAME,WRITE_MODE)
+    
+pickle.dump(full_comment_list,reddit_data_file)
         
 
