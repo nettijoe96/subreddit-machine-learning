10683ffa10c6303f442f2868b6ade22516cb3710
Eric Tiano <etiano417@gmail.com>
2019-11-18 15:36:45 -0500

    added load_videogames
     - refactored load_boardgames by extracting its contents into a parameterized private method
     - added load_videogames using the same private method as load_boardgames

--- loadData.py
+++ loadData.py
@@ -5,25 +5,39 @@ VIDEOGAMES_PICKLE_FILE = "videogames.pickle"
 READ_MODE = "rb"
 
 """
-Load boardgame files into memory
+Load boardgames files into memory
 
 given number n, return a list of the first n stored boardgame comments
 """
 def load_boardgames(num_comments):
+    return __load_pickled_data(num_comments, BOARDGAMES_PICKLE_FILE)
+    
+"""
+Load videogames files into memory
 
-    boardgames_file = open(BOARDGAMES_PICKLE_FILE, READ_MODE)
-    boardgames_comments = pickle.load(boardgames_file)
+given number n, return a list of the first n stored videogames comments
+"""
+def load_videogames(num_comments):
+    return __load_pickled_data(num_comments, VIDEOGAMES_PICKLE_FILE)
     
-    num_comments_loaded = len(boardgames_comments)
     
-    if(num_comments_loaded < num_comments):
+"""
+Load pickled data into memory
+"""
+def __load_pickled_data(num_items, pickle_file_name):
+    pickle_file = open(pickle_file_name, READ_MODE)
+    data_items = pickle.load(pickle_file)
+    
+    num_items_loaded = len(data_items)
+    
+    if(num_items_loaded < num_items):
     
         error_message = "{} items requested, only {} items available".format(
-            num_comments, num_comments_loaded)
+            num_items, num_items_loaded)
             
         raise ValueError(error_message)
     
-    output = boardgames_comments[0:num_comments]
+    output = data_items[0:num_items]
     
     return output
     
