6ea28400e282b7230060db000a1053f17ab381ed
Joe Netti <nettijoe@gmail.com>
2019-12-04 16:05:33 -0500

    renamed the wrong file. fixed it here.

--- svc.py
+++ svc.py
@@ -1,61 +1,34 @@
-import sklearn
+from sklearn import svm
 import utility
-import operator
 
 training = 350
-dev = 175
-testing = 175
-
-
-#some rudimentary feature selection stuff is below
 
 boardComments = utility.load_boardgames(training)
-boardDict = utility.wordFreq(boardComments)
-boardTup = sorted(boardDict.items(), key=operator.itemgetter(1), reverse=1)
-#for i in range(0, 50):
-#    print(boardTup[i][0], boardTup[i][1])
-
-print()
-
 videoComments = utility.load_videogames(training)
-videoDict = utility.wordFreq(videoComments)
-videoTup = sorted(videoDict.items(), key=operator.itemgetter(1), reverse=1)
-#for i in range(0, 50):
-#    print(videoTup[i][0], videoTup[i][1])
 
+bagOfWords = ["people", "game"]
 
+comments = boardComments + videoComments
+X = []
+Y = []
 
-print("top boardgames words that are different from videogames")
-diff = 20
-boardDiff = []
-for i in range(0, 50):
-    word = boardTup[i][0]  
-    boardVal = boardTup[i][1]
-    videoVal = 0
-    if word in videoDict:
-        videoVal = videoDict[word]
-    wordDiff = boardVal - videoVal
-    if wordDiff >= diff:
-        boardDiff += [(word, wordDiff)]
+for c in boardComments:
+    Y += ["board"]
+for c in videoComments:
+    Y += ["video"]
 
-for w in boardDiff:
-    print(w)
+for c in comments:
+   cleaned = utility.cleanComment(c.body)
+   features = []
+   for word in bagOfWords:
+       if word in cleaned:
+           features += [1]
+       else:
+           features += [0]
+   X += [features]
 
+model = svm.SVC(gamma="auto");
+model.fit(X,Y)
 
-print("top videogame words that are different from boardgames\n")
-videoDiff = []
-diff = 20
-for i in range(0, 50):
-    word = videoTup[i][0]  
-    videoVal = videoTup[i][1]
-    boardVal = 0
-    if word in boardDict:
-        boardVal = boardDict[word]
-    wordDiff = videoVal - boardVal
-    if wordDiff >= diff:
-        videoDiff += [(word, wordDiff)]
-
-for w in videoDiff:
-    print(w)
 
 
